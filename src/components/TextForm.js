a55012fc81ca71a1ed10eb267894fb255a729fdf
Mansi-1312 <82mkumari@gmail.com>
2025-09-06 10:45:59 +0530

    App.js,Navbar.js,TextForm.js,Alert.js,About.js,index.css,index.html,About.css

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,9 +18,9 @@ export default function TextForm(props) {
     setText(event.target.value);
   } 
   const handleCopy=()=>{
-    var text=document.getElementById("myBox"); 
-    text.select(); 
-    navigator.clipboard.writeText(text.value); 
+    // var text=document.getElementById("myBox"); 
+    // text.select(); 
+    navigator.clipboard.writeText(text); 
     document.getSelection().removeAllRanges();
     props.showAlert("Copied to clipboard","success");
     
@@ -53,9 +53,9 @@ export default function TextForm(props) {
       <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button> 
      <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear text</button>   
       </div>  
-    <div className="container my-2" style={{color:props.mode ==='dark' ? 'white':'black'}}> 
+    <div className="container my-2" style={{color:props.mode ==='dark' ? 'white':'#042743'}}> 
       <h2>Your text Summary</h2> 
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length} Characters</p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words, {text.length} Characters</p>
       <p>{0.008* text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read </p>
       <h2>Preview</h2> 
       <p>{text.length>0?text:"Nothing to preview!"}</p> 
